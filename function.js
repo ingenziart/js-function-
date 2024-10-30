90ed819472c7d90bad7c5e156cec927e57af7793
ingenziart <104985332+ingenziart@users.noreply.github.com>
2024-10-30 12:13:40 -0400

    nesting function

--- function.js
+++ function.js
@@ -5,3 +5,28 @@ console.log(square(12))
 
 const make_moise=function(){console.log("ping")}
 make_moise
+
+//grobal and local scope 
+//nested function by definning function method
+const study=function(){
+    let studying="by choise"
+    const outside=function(){
+        let gone="like it"
+        console.log(studying)
+        console.log(gone)
+    }
+    outside()
+}
+study()
+
+const years =function(){
+    let my_age=20;
+    const achievement=function(){
+        let property="house"
+    
+        console.log(my_age)
+        console.log(property)
+    }
+    achievement()
+}
+years()
