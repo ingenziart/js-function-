40cdea779b43edd75e905cfe44cef0e8957f1294
ingenziart <104985332+ingenziart@users.noreply.github.com>
2024-10-30 10:31:13 -0400

    function

---
+++ function.js
@@ -0,0 +1,7 @@
+/*functiuon is the way of wrapping programm in value that has many uses, function help us to structure program that helpn us reduce repetion */
+
+const square =function(x){return x*x};
+console.log(square(12))
+
+const make_moise=function(){console.log("ping")}
+make_moise
