dfdd5eb4f56e634e8ed23a72d23d8510eff830e0
ingenziart <104985332+ingenziart@users.noreply.github.com>
2024-10-30 23:17:53 -0400

    exercises on function

---
+++ exerc.js
@@ -0,0 +1,10 @@
+/*1. Write a JavaScript function that reverses a number.
+Example x = 32243;
+Expected Output : 34223*/
+
+
+const reversed=function(n){
+    n=n.toString();//converting n to string.
+    return n.split('').reverse().join('')
+}
+console.log(Number(reversed(32243)))
