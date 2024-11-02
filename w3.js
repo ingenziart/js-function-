3c3e19314761234beb6bc4c615e0ea74c16459fd
ingenziart <104985332+ingenziart@users.noreply.github.com>
2024-11-01 21:33:24 -0400

    litle bit function w3

--- w3.js
+++ w3.js
@@ -4,3 +4,25 @@
 
 //the difference bettwen let,const with var 
 //1 var can be redeclard for example var x=0 and let var x="januzaj" while other can't
+
+const cars=["bmw","lambo","volvo"]
+cars[0]="toyota"
+console.log(cars)
+
+const cars=["bmw","lambo","volvo"]
+cars.push("audi")
+console.log(cars)
+
+//function
+
+let x=myfunction(3,4)
+function myfunction(a,b){
+    return a*b
+}
+console.log(x)
+
+// another way.
+function myfunction(a,b){
+    return a*b
+}
+console.log(myfunction(3,4))
