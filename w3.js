399879d8c1a66daa009b57fab7f56d12d10f4887
ingenziart <104985332+ingenziart@users.noreply.github.com>
2024-11-03 14:40:09 -0500

    another method flat

--- w3.js
+++ w3.js
@@ -200,3 +200,10 @@ console.log(m)
 const myfluits=["Banana","pear","mango","brueberry"]
 let t=myfluits.join(" ")
 console.log(t)
+
+//arrray flat
+
+const myArr = [[1,2],[3,4],[5,6]];
+let s=myArr.flat()
+let o=s.toString()
+console.log(o)
