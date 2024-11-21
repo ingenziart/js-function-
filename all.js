052bae3af4cd3adebbc9ec5ce9bc6960d6e533a3
ingenziart <104985332+ingenziart@users.noreply.github.com>
2024-11-21 16:47:54 -0500

    recall

--- all.js
+++ all.js
@@ -82,3 +82,11 @@ console.log(me)
 const date= new Date()
 x=date.getDay()
 console.log(x)
+
+
+const number=[1,2,3,4,5]
+let numbe=""
+for(let i in number){
+    numbe+=number[i]+" "
+}
+console.log(numbe)
