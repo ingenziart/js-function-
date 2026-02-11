3cee6c23bb6a29b7216887be950ed37c1d81c29a
ingenziart <104985332+ingenziart@users.noreply.github.com>
2026-02-11 14:39:01 -0500

    revising js with react way of gin way

---
+++ main.js
@@ -0,0 +1,10 @@
+import { runVariables } from "./variables/variable.js";
+
+
+
+function startApp(){
+    console.log("revise js");
+    runVariables
+    
+}
+startApp() 
---
+++ variables/variable.js
@@ -0,0 +1,18 @@
+
+
+
+export function runVariables(){
+    let score =20
+    const appName="janu"
+
+    const user={name:"mbabazi"}
+
+    user.name="aime"
+
+    console.log(score)
+    console.log(appName)
+    console.log(user);
+
+    
+}
+runVariables()
