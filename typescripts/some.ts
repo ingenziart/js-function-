0eaa46154edc6c0eca86b3f3557819b921a3c771
ingenziart <104985332+ingenziart@users.noreply.github.com>
2026-02-23 19:01:34 -0500

    typescript reviews

--- typescripts/some.ts
+++ typescripts/some.ts
@@ -1,8 +1,11 @@
- type User={
-        name:string
-        age :number
-        isAdmin:boolean
-    }
+
+interface User {
+  name: string
+  age: number
+  isAdmin: boolean
+  email?: string  // optional
+}
+
 export function runTypescript(){
    
     const user:User={
@@ -20,15 +23,26 @@ export function runTypescript(){
      printUser(user)
 
      const users:User[]=[
-        {name:"aime",age:24,isAdmin:true},
+        {name:"aime",age:24,isAdmin:true,email:"hhh"},
         {name:"janu",age:26,isAdmin:false}
      ]
+
     //console.log(users);
 
     const names =users.map((username)=> username.name)
    //console.log(names);
 
    const filtering=users.filter((user)=>user.isAdmin)
-   console.log(filtering);
+   //console.log(filtering);
+   //console.log(users);
+
+   function getName(user:User): number {
+    return user.age
+
+    
+   }
+   console.log(getName(user));
+
+
    
 }
